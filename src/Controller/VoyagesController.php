aa8d584be15013a31187995b70f1f727dc700338
suki33 <betulcolak361@gmail.com>
2022-01-11 23:57:47 +0100

    Phase 10 : ORM (filtrage des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 /**
  * Description of VoyagesController
@@ -36,14 +37,32 @@ class VoyagesController extends AbstractController{
             'visites' => $visites
         ]);
     }
-        /**
+    /**
          * @Route("/voyages/tri/{champ}/{ordre}", name="voyages.sort")
          * @param type $champ
          * @param type $ordre
          * @return Response
          */
-    public function sort($champ, $ordre): Response{
-            $visites = $this->repository->findAllOrderBy($champ, $ordre);
+    public function sort($champ, $ordre):Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites'=> $visites
+        ]);
+    }
+    
+    
+ 
+    
+    
+        /**
+         * @Route("/voyages/recherche/{champ}", name="voyages.findallequal")
+         * @param type $champ
+         * @param Request $request
+         * @return Response
+         */
+    public function findAllEqual($champ, Request $request): Response{
+            $valeur = $request->get("recherche");
+            $visites = $this->repository->findByEqualValue($champ, $valeur);
             return $this->render("pages/voyages.html.twig", [
               'visites' => $visites
         ]);
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -19,21 +19,42 @@ class VisiteRepository extends ServiceEntityRepository
         parent::__construct($registry, Visite::class);
     }
 
-
-
+    
+    
 /**
- * Retourne toutes les visites triees sur un champs
+ * Retourne toutes les visitess triees sur un champ
  * @param type $champ
  * @param type $ordre
  * @return Visite[]
  */
 
- 
-public function findAllOrderBy($champ, $ordre): array{
+ public function findAllOrderBy($champ, $ordre): array {
     return $this->createQueryBuilder('v')
             ->orderBy('v.'.$champ, $ordre)
+            ->getQuery()
+            ->getResult(); 
+ }
+ /**
+ * Enregistrements dont un champ est egal a une valeur 
+ * ou tous les enregistrements si la valeur est vide
+ * @param type $champ
+ * @param type $valeur
+ * @return Visite[]
+ */
+public function findByEqualValue($champ, $valeur): array {
+    if($valeur==""){
+    return $this->createQueryBuilder('v')//alias de la table
+            ->orderBy('v.'.$champ, 'ASC')
+            ->getQuery()
+            ->getResult(); 
+
+    }else{
+        return $this->createQueryBuilder('v')//alias de la table
+            ->where('v.'.$champ.'=:valeur')
+            ->setParameter('valeur', $valeur)
+            ->orderBy('v.datecreation','DESC')
             ->getQuery()
             ->getResult();
     }
 }
-    
+}
